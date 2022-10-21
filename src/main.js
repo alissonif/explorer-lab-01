0e69690bfa51214fc00eac726dd9ac44b14eaa4a
José Alisson <josealissonif@gmail.com>
2022-10-21 10:43:03 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import IMask from 'imask';
 
 const ccBgColor1 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor2 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
-const ccLogo = document.querySelector(".cc-logo spann:nth-child(2) img")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
 function setCardType(type) {
 
@@ -16,6 +16,8 @@ function setCardType(type) {
 
   ccBgColor1.setAttribute('fill', colors[type][0])
   ccBgColor2.setAttribute('fill', colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+
 }
 
 globalThis.setCardType = setCardType
